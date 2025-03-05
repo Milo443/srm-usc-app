ab1f834fcafea7d753a647f719ad3521077a9b12
Milo443 <104799754+Milo443@users.noreply.github.com>
2025-03-05 18:21:31 -0500

    view login

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
 
-const routes: Array<RouteRecordRaw> = [
+import HomePage from '../views/HomePage.vue'
+import Login from '../views/Login.vue';
+
+const routes = [
   {
     path: '/',
     redirect: '/home'
@@ -11,6 +12,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/home',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <!-- <ion-toolbar>
+                <ion-title>Login</ion-title>
+            </ion-toolbar> -->
+        </ion-header>
+
+        <ion-content>
+            <ion-card>
+                <ion-card-header>
+                    <ion-card-title>Login</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                    <ion-list lines="none">
+                        <ion-item>
+                            <ion-input
+                            label="Email"
+                            label-placement = "stacked"
+                            placeholder="alan.brito00@usc.edu.co"
+                            ></ion-input>
+                        </ion-item>
+                        <ion-item>
+                            <ion-input
+                            type = 'password'
+                            label= "Password"
+                            label-placement = "stacked">
+                                <ion-input-password-toggle slot="end"></ion-input-password-toggle>
+                            </ion-input>
+                        </ion-item>
+                        <ion-item>
+                            <ion-button  expand="block" shape="round">Login</ion-button>
+                        </ion-item>
+                    </ion-list>
+                    
+
+                    
+                </ion-card-content>
+            </ion-card>
+        </ion-content>
+
+    </ion-page>
+</template>
