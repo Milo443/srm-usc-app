31c34d89615f83fc87550c9d119b34d62536d6c9
Milo443 <104799754+Milo443@users.noreply.github.com>
2025-03-31 20:00:01 -0500

    Configuración inicial de Firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,69 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, {useState} from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonButton, IonToast } from '@ionic/react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useHistory } from 'react-router-dom';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
+  const history = useHistory();
+  const auth = getAuth();
+
+  const register = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+      console.log('Usuario registrado con éxito');
+      history.push('/home');
+    } catch (error: any) {
+      setToastMessage(error.message);
+      setShowToast(true);
+    }
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Registrarse</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding">
+        <form onSubmit={register}>
+          <IonItem>
+            <IonLabel position="floating">Correo electrónico</IonLabel>
+            <IonInput
+              type="email"
+              value={email}
+              onIonChange={e => setEmail(e.detail.value!)}
+              required
+            />
+          </IonItem>
+
+          <IonItem>
+            <IonLabel position="floating">Contraseña</IonLabel>
+            <IonInput
+              type="password"
+              value={password}
+              onIonChange={e => setPassword(e.detail.value!)}
+              required
+            />
+          </IonItem>
+
+          <IonButton className="ion-margin-top" type="submit" expand="block">
+            Registrarse
+          </IonButton>
+        </form>
+
+        <IonToast
+          isOpen={showToast}
+          onDidDismiss={() => setShowToast(false)}
+          message={toastMessage}
+          duration={3000}
+        />
       </IonContent>
     </IonPage>
   );
