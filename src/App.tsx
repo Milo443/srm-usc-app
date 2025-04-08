73b49ba2bf0d37847883e690f914a3888511e0c2
camilo calderon <calderon-1@outlook.com>
2025-04-07 20:21:36 -0500

    implement page register

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -42,8 +44,12 @@ const App: React.FC = () => (
         <Route exact path="/home">
           <Home />
         </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/register">
+          <Register />
+          {/* <Redirect to="/home" /> */}
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
