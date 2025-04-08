e8c2a79e505f179d164bfc6cb9fb462df08f31d5
estaban penaranda <estebanpc544@gmail.com>
2025-04-07 20:45:24 -0500

    login implementacion

---
+++ src/pages/Login.tsx
@@ -0,0 +1,79 @@
+import React, {useState} from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonLabel, IonInput, IonButton, IonToast } from '@ionic/react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { useHistory, Link } from 'react-router-dom';
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showToast, setShowToast] = useState(false);
+  const [toastMessage, setToastMessage] = useState('');
+  const history = useHistory();
+  const auth = getAuth();
+
+  const register = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+        const token= await user.getIdToken;
+
+        console.log('Token: ', token);
+
+        history.push('/home');
+    } catch (error: any) {
+      setToastMessage(error.message);
+      setShowToast(true);
+    }
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Iniciar sesion</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <form onSubmit={register}>
+          <IonItem>
+            <IonLabel position="floating">Correo electrónico</IonLabel>
+            <IonInput
+              type="email"
+              value={email}
+              onIonChange={e => setEmail(e.detail.value!)}
+              required
+            />
+          </IonItem>
+
+          <IonItem>
+            <IonLabel position="floating">Contraseña</IonLabel>
+            <IonInput
+              type="password"
+              value={password}
+              onIonChange={e => setPassword(e.detail.value!)}
+              required
+            />
+          </IonItem>
+
+          <IonButton className="ion-margin-top" type="submit" expand="block">
+            Iniciar Sesion
+          </IonButton>
+        </form>
+
+            <IonButton className="ion-margin-top" fill='outline' expand="block">
+                    <Link to="/register" style={{textDecoration: 'none', color:'inherit'}} >Crear cuenta</Link>
+            </IonButton>
+
+        <IonToast
+          isOpen={showToast}
+          onDidDismiss={() => setShowToast(false)}
+          message={toastMessage}
+          duration={3000}
+        />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
